2dcc744535d267c7dcd7c5dbe3c33c7225b7f68b
Jenkins <t.l.jenkins@exeter.ac.uk>
2025-02-02 09:20:48 +0000

    add portfolio items widget feature

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,26 +8,37 @@
 const portfolioBtnContainer = document.querySelector(".portfolio__btn--container");
 const portfolioBtns = document.querySelectorAll(".portfolio__btn");
 const portfolioBtnAll = document.querySelector(".portfolio__btn--1");
+const portfolioItems = document.querySelectorAll(".portfolio__item");
 
 // When page loads by default highlight the "ALL" button
 document.addEventListener("DOMContentLoaded", () => {
     portfolioBtnAll.classList.add("portfolio__btn--highlight");
 });
 
-// Highlight the button the user clicks and remove highlight from other button
+// Highlight the button the user clicks and subset portfolio items
 portfolioBtnContainer.addEventListener("click", (e) => {
     
     if (e.target.classList.contains("portfolio__btn")) {
 
-        // remove highlight from all buttons
+        // remove highlight class from all buttons
         portfolioBtns.forEach(btn => {
             btn.classList.remove("portfolio__btn--highlight");
         });
 
-        // identify which button was clicked and add highlight
-        const buttonClicked= `portfolio__btn--${e.target.dataset.id}`;
-        const buttonClickElement = document.querySelector(`.${buttonClicked}`);
-        buttonClickElement.classList.add("portfolio__btn--highlight")
+        // remove all portfolio items from DOM
+        portfolioItems.forEach(item => item.classList.add("portfolio__item--hide"));
+
+        // identify which button was clicked and add highlight class
+        const buttonClicked = e.target.dataset.id;
+        const buttonClickElement = document.querySelector(`.portfolio__btn--${buttonClicked}`);
+        buttonClickElement.classList.add("portfolio__btn--highlight");
+
+        // identify and display categories that belong to button clicked on DOM
+        portfolioItems.forEach(item => {
+            
+            if (item.dataset.category.split(" ").includes(buttonClicked)) {
+                item.classList.remove("portfolio__item--hide");
+            };
+        });
     };
 });
-
