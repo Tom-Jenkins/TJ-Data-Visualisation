ae441c544a393dd7c4d2c408f0e9459aceeaaedf
Jenkins <t.l.jenkins@exeter.ac.uk>
2025-02-03 15:39:18 +0000

    update website

--- js/home.js
+++ js/home.js
@@ -6,7 +6,7 @@
 
 // Intitalise map and add tiles
 const coords = [50.734135114897754, -3.533123160443835];
-const map = L.map("map").setView(coords, 6);
+const map = L.map("map").setView(coords, 4);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     crs: L.CRS.EPSG3857,
     maxZoom: 13,
